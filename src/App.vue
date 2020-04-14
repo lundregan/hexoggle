7c15a9fbb1aa8f1022b3f297bec24bcfa25abf79
lundregan <lundregan.ethan@gmail.com>
2020-04-14 21:57:08 +0000

    Made a hexagon grid

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,74 @@
 <template>
   <div id="app">
-
+    <div class="hexagon-div">
+      <div class="row">
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+      </div>
+      <div class="row">
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+      </div>
+      <div class="row">
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+      </div>
+      <div class="row">
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+      </div>
+      <div class="row">
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+        <svg height="200" width="174">
+          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,5 +89,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.hexagon-svg {
+  fill:grey;
+  stroke-width:1;
+  border: 1px solid black;
+}
+
+.row {
+  margin-top: -56px;
+}
+
+.row svg {
+  margin-left: 10px;
+  margin-top: 10px;
 }
 </style>

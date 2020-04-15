d6c3cee642abeba21df1bbe03a2a0448925cbc0b
lundregan <lundregan.ethan@gmail.com>
2020-04-15 13:09:30 +0000

    Hexagons SVG now within a viewbox

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,79 @@
 <template>
   <div id="app">
 
-    <h1>Hexaoggle</h1>
+    <h1 class='title'>Hexoggle</h1>
 
-    <div class="hexagon-div">
-      <div class="row">
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-      </div>
-      <div class="row">
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-      </div>
-      <div class="row">
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-      </div>
-      <div class="row">
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-      </div>
-      <div class="row">
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-        <svg height="200" width="174">
-          <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
-        </svg>
-      </div>
-    </div>
+    <svg viewBox='0 0 1000 1000' class='exa'>
+      <!-- 1st row -->
+      <svg x='200' y='0'>
+        <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='400' y='0'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='600' y='0'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+
+      <!-- 2nd row -->
+      <svg x='100' y='175'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='300' y='175'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='500' y='175'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='700' y='175'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+
+      <!-- 3rd row -->
+      <svg x='0' y='350'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='200' y='350'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='400' y='350'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='600' y='350'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='800' y='350'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+
+      <!-- 4th row -->
+      <svg x='100' y='525'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='300' y='525'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='500' y='525'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      <svg x='700' y='525'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      
+      <!-- 5th row -->
+      <svg x='200' y='700'>
+        <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+      
+      <svg x='400' y='700'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+
+      <svg x='600' y='700'>
+         <polygon class='hexagon-svg' points="87,0 174,50 174,150 87,200 0,150 0,50 87,0" />
+      </svg>
+
+    </svg>
   </div>
 </template>
 
@@ -91,17 +93,37 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  margin: 0;
+}
+
+.exa {
+  width: 40vw;
+  height: 40vw;
+}
+
+.title {
+  margin-top: 0px;
+
+  padding-bottom: 10px;
+
+  font-size: 3rem;
+
+  background-color: #2e2e2e;
+  color:            #c0c0c0;
 }
 
 .hexagon-div {
   margin-top: 56px; /* Fixes negative margin given to each row */
+
+  padding-top: 20px;
 }
 
 .hexagon-svg {
   fill:grey;
   stroke-width:1;
-  border: 1px solid black;
 }
 
 .row {
